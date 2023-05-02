73b53943dc766eec3e142f4240a3643f8778158d
Alena Roshchyna <girls.code.better@gmail.com>
2023-05-02 18:15:26 +0200

    Save copy

--- src/App.js
+++ src/App.js
@@ -1,12 +1,9 @@
-/* import './App.css'; */
+import './App.css';
 
 export default function App() {
-  const generateColor = () => {
-    setColor(Math.random().toString(16).substr(-6));
-  };
   return (
     <div className="App">
-      <div>Generated Color: {generateColor}</div>
+      <div>Generated Color:</div>
       <button>Generate</button>
     </div>
   );
