9b665462ca8584fb9f77830548f5a4117ae9fb1b
Alena Roshchyna <girls.code.better@gmail.com>
2023-05-03 14:29:17 +0200

    Add library and hex, and func button

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import './App.css';
+import randomColor from 'randomcolor';
+import { useState } from 'react';
 
 export default function App() {
+  const [color, setColor] = useState(randomColor);
+
   return (
     <div className="App">
-      <div>Generated Color:</div>
-      <button>Generate</button>
+      <div style={{ background: color }}>Generated Color: {color}</div>
+      <button onClick={() => setColor(randomColor)}>Generate</button>
     </div>
   );
 }
