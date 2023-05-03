b07056db9516aacecd2e5c32fbc0811fbd86d2e7
Alena Roshchyna <girls.code.better@gmail.com>
2023-05-03 16:33:10 +0200

    Add label and played with css

--- src/App.js
+++ src/App.js
@@ -5,21 +5,31 @@ import { useState } from 'react';
 export default function App() {
   const [color, setColor] = useState(randomColor);
   const [hueColor, setHueColor] = useState('');
+  const [luminosityColor, setLuminosityColor] = useState('');
   return (
     <div className="App">
       <div style={{ background: color }}>Generated Color: {color}</div>
       <button
         onClick={() =>
-          setColor(randomColor({ hue: hueColor, luminosity: 'light' }))
+          setColor(randomColor({ hue: hueColor, luminosity: luminosityColor }))
         }
       >
         Generate
       </button>
       <br />
+      <label htmlFor="hueColor">Hue:</label>
       <input
         value={hueColor}
         onChange={(event) => {
           setHueColor(event.currentTarget.value);
+        }}
+      />
+      <br />
+      <label htmlFor="luminosityColor">Luminosity:</label>
+      <input
+        value={luminosityColor}
+        onChange={(event) => {
+          setLuminosityColor(event.currentTarget.value);
         }}
       />
     </div>
