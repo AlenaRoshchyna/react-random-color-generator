7ee5f1cbcff90605337193f4d728f3faa1f8ec63
Alena Roshchyna <girls.code.better@gmail.com>
2023-05-03 15:47:43 +0200

    Add hue

--- src/App.js
+++ src/App.js
@@ -4,11 +4,31 @@ import { useState } from 'react';
 
 export default function App() {
   const [color, setColor] = useState(randomColor);
-
+  const [hueColor, setHueColor] = useState('');
   return (
     <div className="App">
       <div style={{ background: color }}>Generated Color: {color}</div>
-      <button onClick={() => setColor(randomColor)}>Generate</button>
+      <button
+        onClick={() =>
+          setColor(randomColor({ hue: hueColor, luminosity: 'light' }))
+        }
+      >
+        Generate
+      </button>
+      <br />
+      <input
+        value={hueColor}
+        onChange={(event) => {
+          setHueColor(event.currentTarget.value);
+        }}
+      />
     </div>
   );
 }
+
+/* const hue = process.argv[2];
+const luminosity = process.argv[3];
+const color = randomColor({
+  luminosity: luminosity,
+  hue: hue,
+}); */
